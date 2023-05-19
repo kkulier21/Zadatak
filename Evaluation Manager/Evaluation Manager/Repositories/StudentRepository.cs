b56c214b314fffa77ed551ee79fc8c560f7d7a34
Karla Kulier <38597@DESKTOP-4Q3G9S3>
2023-05-19 18:16:22 +0200

    Kreiran StudentRepository

---
+++ Evaluation Manager/Evaluation Manager/Repositories/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using DBLayer;
+using Evaluation_Manager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repositories
+{
+    public class StudentRepository
+    {
+        public static Student GetStudent(int id)
+        {
+            Student student = null;
+            string sql = $"SELECT * FROM Students WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                student = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return student;
+        }
+        public static List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            string sql = "SELECT * FROM Students";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Student student = CreateObject(reader);
+                students.Add(student);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return students;
+        }
+
+
+    }
+}
