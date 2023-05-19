ad0641d1cd6b01a1e9025e1b118d81797d5f434b
Karla Kulier <38597@DESKTOP-4Q3G9S3>
2023-05-19 19:45:29 +0200

    Prijava povezana s bazom

--- Evaluation Manager/Evaluation Manager/Form1.cs
+++ Evaluation Manager/Evaluation Manager/Form1.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,8 +14,8 @@ namespace Evaluation_Manager
 {
     public partial class FrmLogin : Form
     {
-        string username = "nastavnik";
-        string password = "test";
+        public static Teacher LoggedTeacher { get; set; }
+
         public FrmLogin()
         {
             InitializeComponent();
@@ -43,7 +45,8 @@ namespace Evaluation_Manager
             }
             else
             {
-                if (txtUsername.Text == username && txtPassword.Text == password)
+                LoggedTeacher = TeacherRepository.GetTeacher(txtUsername.Text);
+                if (LoggedTeacher != null && LoggedTeacher.Password == txtPassword.Text)
                 {
                     dgvStudents frmStudents = new dgvStudents();
                     Hide();
